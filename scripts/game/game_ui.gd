7c0ca9de4b03c91b389a6c5ef98813d73530492b
createsource <abnormalhare822@gmail.com>
2026-04-22 11:20:07 -0400

    fix chat

--- scripts/game/game_ui.gd
+++ scripts/game/game_ui.gd
@@ -4,8 +4,8 @@ signal send_message
 @onready var Chat = $Game/Multplayer/Chat/ChatLog;
 
 func append_chat_log(text: String):
-	Chat.text += text;
-	Chat.scroll_vertical = Chat.get_line_count()
+	Chat.text += text + "\n";
+	Chat.scroll_vertical = INF;
 
 func _on_message_send_message(text: String) -> void:
 	send_message.emit(text);
